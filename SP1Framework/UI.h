b1a905d3bbe14ef7eeb7eb7c34a55b5819edb183
TemplarDon <donovandenzel@gmail.com>
2015-08-20 09:56:02 +0800

    UI again. previous try went wrong

---
+++ SP1Framework/UI.h
@@ -0,0 +1,35 @@
+#ifndef _UI_H
+#define _UI_H
+#include <string>
+using std::string;
+
+struct player
+{
+    int lives;
+    char inventory[6];
+    string inventory0;
+    string inventory1;
+    string inventory2;
+    string inventory3;
+    string inventory4;
+    string inventory5;
+    int points;
+};
+
+enum items
+{
+    item1 = 1,
+    item2,
+    item3,
+    item4,
+    item5,
+    item6,
+};
+
+void timer (double g_dElapsedTime);
+void lives (player & user);
+//void damagetaken ( player user, bool damage );
+void renderInventory ( player & user );
+void point( player & user );
+
+#endif // _UI_H
