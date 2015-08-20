5ff793bb8e752e0778f2b616d6839a16386a8e03
darth343 <darth343@gmail.com>
2015-08-20 11:51:33 +0800

    Resolved conflicts with Don and Arun

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -15,10 +15,16 @@ using std::endl;
 double elapsedTime;
 double deltaTime;
 bool keyPressed[K_COUNT];
+bool flipswitch1 = 0;
+bool flipswitch2 = 0;
 COORD charLocation;
 COORD consoleSize;
 
 PMAP MapCollision;
+//Start Health
+int health = 5;
+int lastX = 0;
+int lastY = 0;
 
 void init()
 {
@@ -98,18 +104,18 @@ void update(double dt, player & user)
 
     // Updating the location of the character based on the key press
 	// providing a beep sound whenver we shift the character
-    if (keyPressed[K_UP] && charLocation.Y > 0)
+    if (keyPressed[K_UP] && charLocation.Y > 0 && health > 0)
     {
         Beep(1440, 30);
-		if(MapCollision->data[charLocation.Y - 1][charLocation.X] != 'B')
+		if(MapCollision->data[charLocation.Y - 1][charLocation.X] != 'W')
 		{
 			charLocation.Y--;
 		}		
     }
-    if (keyPressed[K_LEFT] && charLocation.X > 0)
+    if (keyPressed[K_LEFT] && charLocation.X > 0 && health > 0)
     {
         Beep(1440, 30);
-		if(MapCollision->data[charLocation.Y][charLocation.X - 1] != 'B')
+		if(MapCollision->data[charLocation.Y][charLocation.X - 1] != 'W')
 		{
 			charLocation.X--;
 		}
@@ -119,38 +125,30 @@ void update(double dt, player & user)
 	//	Beep(1440, 30);
 	//	g_cCharLocation.X += g_cConsoleSize.X -1; 
 	//}
-    if (keyPressed[K_DOWN] && charLocation.Y < consoleSize.Y - 1)
+    if (keyPressed[K_DOWN] && charLocation.Y < consoleSize.Y - 1 && health > 0)
     {
         Beep(1440, 30);
-		if(MapCollision->data[charLocation.Y + 1][charLocation.X] != 'B')
+		if(MapCollision->data[charLocation.Y + 1][charLocation.X] != 'W')
 		{
-			charLocation.Y++;
+			if(MapCollision->data[charLocation.Y+1][charLocation.X] != 'X')
+			{
+				charLocation.Y++;
+			}else if(flipswitch1 == 1)
+			{
+				charLocation.Y++;
+			
 		}	
     }
-    if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 1)
+    if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 1 && health > 0)
     {
         Beep(1440, 30);
-		if(MapCollision->data[charLocation.Y][charLocation.X + 1] != 'B')
+		if(MapCollision->data[charLocation.Y][charLocation.X + 1] != 'W')
 		{
 			charLocation.X++;
 		}
  
     }
-	//if(g_abKeyPressed[K_RIGHT] && g_cCharLocation.X == g_cConsoleSize.X - 1)
-	//{
-	//	Beep(1440, 30);
-	//	g_cCharLocation.X = 0; 
-	//}
-	//if(g_abKeyPressed[K_TEST] && g_abKeyPressed[K_RIGHT] && g_cCharLocation.X < g_cConsoleSize.X - 1)
-	//{
-	//	Beep(1440, 30);
-	//	g_cCharLocation.X += 10;
-	//	while(g_cCharLocation.X > g_cConsoleSize.X -1)
-	//	{
-	//		g_cCharLocation.X--;
-	//	}
-	//}
-    
+
     // quits the game if player hits the escape key
     if (keyPressed[K_ESCAPE])
         g_quitGame = true;
@@ -158,8 +156,8 @@ void update(double dt, player & user)
     // quits if player lives at 0
     if (user.lives == 0)
     {
-        g_quitGame = true;
-    }
+        g_quitGame = true;    
+}
 
     //TEST FOR DAMAGE
     if (keyPressed[K_UP])
@@ -197,9 +195,49 @@ void update(double dt, player & user)
 //--------------------------------------------------------------
 void render( player & user )
 {
-	createMap(charLocation, 1, 10);
     
     //UI functions
+	createMap(charLocation, 1, 6, flipswitch1);
+	/*if(MapCollision->data[charLocation.Y][charLocation.X] == '1')
+	{
+		flipswitch1 = 1;
+		gotoXY(50, 4);
+		cout << "YOU Activated X Switch!";
+	}
+	for(int i=0; i < health; ++i)
+	{
+		cout << "<3 ";
+	}
+
+	gotoXY(50, 3);
+	if(MapCollision->data[charLocation.Y][charLocation.X] == 'D' && health > 0)
+	{
+		cout << "You have been hurt!" << endl;
+		if(lastX != charLocation.X)
+		{
+		health--;
+		lastX = charLocation.X;
+		}else if(lastY != charLocation.Y)
+		{
+		health--;
+		lastY = charLocation.Y;
+		}
+	}else if(MapCollision->data[charLocation.Y][charLocation.X] == 'L' && health > 0)
+	{
+		health-=health;
+	}else
+	{
+		lastX = charLocation.X;
+		lastY = charLocation.Y;
+	}
+	if(health <= 0)
+	{
+		gotoXY(50, 4);
+		cout << "YOU DIED!";
+		Beep(2000, 1000);
+	} */
+    // render time taken to calculate this frame
+
     timer(elapsedTime);
 
     lives( user );
